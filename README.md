566e421a3b2c9cefcd6640d12676bbcebc875805
IsaacPar <isaacvgm1@gmail.com>
2022-09-18 17:41:27 -0300

    More message customisation is avaliable!

---
+++ README.md
@@ -0,0 +1,18 @@
+# DEV-WEBUI
+
+A simple example of how you can code a Console Web UI
+
+# Download:
+
+1. `git clone https://github.com/IsaacPar/DEV-WEBUI/` ( requires Git )
+2. `npm install` ( requires Nodejs )
+3. Refer to the [Usage](#usage)
+
+# Usage:
+
+1. Start Server with NodeJS : ` npm start `
+2. Read it from the command line with `curl 127.0.0.1:5556` ( requires Curl )
+
+Good! Now you can see a beaultiful output in your console!
+
+![Like that one!](https://user-images.githubusercontent.com/88511157/190925299-854fb9ea-8723-442d-a550-04f489898ed3.png)
--- main.js
+++ main.js
@@ -4,21 +4,41 @@ import cors from "cors"
 
 let app = express()
 
+let o = fs.readFileSync("message.ansi", "utf8")
+
+let rm = (str, replace, replaced) => {
+    while (str.indexOf(replace) !== -1) {
+        str = str.replace(replace, replaced)
+    }
+    return str
+}
+
+let parseTemplate = (string) => {
+    let matches = string.match(/\$\{(.*?)\}/g)
+    matches.forEach((match) => {
+        string = string.replace(match, eval(match.substring(2, match.length - 1)))
+    })
+    return string
+}
+
+o = rm(o, "\\u001b", "\u001b")
+o = rm(o, "\\n", "\n")
+
+let printText = `a`.replace("a", o)
+printText = parseTemplate(printText)
+
 app.use(cors())
 
 app.get('/', (req, res) => {
     console.log("Acessed")
     if (req.headers["user-agent"].includes("curl") || req.headers["user-agent"].includes("wget") ) {
-        res.send(`
-            \u001b[34m------------\u001b[0m
-            \u001b[33;1m \u001b[7mDATA WEBUI\u001b[0m
-            \u001b[34m------------\u001b[0m\n
-
-        \u001b[7mAcessed on:  \u001b[4m${new Date().toLocaleDateString()}\u001b[0m
-        `)
+        res.send(printText)
     } else {
         res.send(fs.readFileSync("fallback.html", "utf8"))
     }
 })
 
-app.listen(5556)
+app.listen(5556, () => {
+    console.log("listening on port 5556")
+    console.log("Here: \n", printText)
+})
